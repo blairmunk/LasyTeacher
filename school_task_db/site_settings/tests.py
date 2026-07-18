a4672b6082748ca4f3ae45708b14120fedaba70f
Alexander L. <lemportpro@mail.ru>
2026-07-18 12:12:23 +0300

    Use explicit site settings form flow

--- school_task_db/site_settings/tests.py
+++ school_task_db/site_settings/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from site_settings.models import SiteSettings
+
+
+class SiteSettingsViewTests(TestCase):
+    def test_get_creates_singleton_settings_context(self):
+        response = self.client.get(reverse('site_settings:index'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['object'].pk, 1)
+        self.assertIsInstance(response.context['form'].instance, SiteSettings)
+
+    def test_post_updates_settings(self):
+        response = self.client.post(
+            reverse('site_settings:index'),
+            data={
+                'school_name': 'Лицей',
+                'teacher_name': 'Иванова М. И.',
+                'default_subject': 'Физика',
+                'current_academic_year': '2026-2027',
+                'points_scale': 100,
+                'default_variants_count': 4,
+                'pdf_font_size': 12,
+                'pdf_margin_top': 15,
+                'pdf_margin_bottom': 15,
+            },
+        )
+
+        settings = SiteSettings.get()
+        self.assertRedirects(
+            response,
+            reverse('site_settings:index'),
+            fetch_redirect_response=False,
+        )
+        self.assertEqual(settings.school_name, 'Лицей')
+        self.assertEqual(settings.default_variants_count, 4)
--- school_task_db/site_settings/views.py
+++ school_task_db/site_settings/views.py
@@ -1,19 +1,32 @@
-from django.views.generic import UpdateView
-from django.urls import reverse_lazy
+from django.shortcuts import redirect
+from django.views.generic import TemplateView
 from django.contrib import messages
 from .models import SiteSettings
 from .forms import SiteSettingsForm
 
 
-class SiteSettingsView(UpdateView):
-    model = SiteSettings
-    form_class = SiteSettingsForm
+class SiteSettingsView(TemplateView):
     template_name = 'site_settings/settings.html'
-    success_url = reverse_lazy('site_settings:index')
 
-    def get_object(self, queryset=None):
-        return SiteSettings.get()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        settings = kwargs.get('object') or SiteSettings.get()
+        context['object'] = settings
+        context['form'] = kwargs.get('form') or SiteSettingsForm(instance=settings)
+        return context
 
-    def form_valid(self, form):
-        messages.success(self.request, 'Настройки сохранены!')
-        return super().form_valid(form)
+    def post(self, request, *args, **kwargs):
+        settings = SiteSettings.get()
+        form = SiteSettingsForm(
+            request.POST,
+            request.FILES,
+            instance=settings,
+        )
+        if not form.is_valid():
+            return self.render_to_response(
+                self.get_context_data(form=form, object=settings),
+            )
+
+        form.save()
+        messages.success(request, 'Настройки сохранены!')
+        return redirect('site_settings:index')

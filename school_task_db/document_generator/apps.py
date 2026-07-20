c76cfda9f3fd06182964241d0346b0ce7d9f713d
Alexander L. <lemportpro@mail.ru>
2026-07-20 16:24:19 +0300

    Rename document template app config class

--- school_task_db/document_generator/apps.py
+++ school_task_db/document_generator/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class DocumentGeneratorConfig(AppConfig):
+class DocumentTemplateConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'document_generator'
     verbose_name = 'Document Templates'

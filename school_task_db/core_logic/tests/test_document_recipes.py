40d2d11914ed47a12533366f9c8130572fb56a3f
Alexander L. <lemportpro@mail.ru>
2026-07-20 17:43:28 +0300

    Move document recipe factory tests

--- school_task_db/core_logic/tests/test_document_recipes.py
+++ school_task_db/core_logic/tests/test_document_recipes.py
@@ -1,24 +1,11 @@
 from unittest import TestCase
 
-from core_logic.value_objects.document_render_options import (
-    RemedialSheetBuildOptions,
-    WorkDocumentBuildOptions,
-)
-from core_logic.value_objects.document_recipe_factories import (
-    build_remedial_sheet_document_recipe,
-    build_work_document_recipe,
-)
 from core_logic.value_objects.document_recipes import (
     ANSWERS_SECTION,
-    FULL_SOLUTIONS_SECTION,
     HEADER_SECTION,
     REMEDIAL_SHEET_DOCUMENT_TYPE,
     TASK_LIST_SECTION,
-    WORK_DOCUMENT_TYPE,
     WORKSHEET_DOCUMENT_TYPE,
-    ORIGINAL_MISTAKES_SECTION,
-    SHORT_SOLUTIONS_SECTION,
-    TRAINING_TASKS_SECTION,
     build_document_recipe_from_sections_config,
     build_document_template_spec_from_config,
 )
@@ -121,87 +108,4 @@ class DocumentRecipeTests(TestCase):
         self.assertEqual(
             template.to_recipe().document_type,
             WORKSHEET_DOCUMENT_TYPE,
-        )
-
-    def test_default_work_recipe_contains_header_and_task_list(self):
-        recipe = build_work_document_recipe()
-
-        self.assertEqual(recipe.document_type, WORK_DOCUMENT_TYPE)
-        self.assertEqual(
-            recipe.section_types,
-            (HEADER_SECTION, TASK_LIST_SECTION),
-        )
-        self.assertEqual(
-            recipe.sections[1].options,
-            {
-                'include_hints': False,
-                'include_instructions': False,
-            },
-        )
-
-    def test_work_recipe_maps_answer_options_to_sections(self):
-        recipe = build_work_document_recipe(
-            WorkDocumentBuildOptions(
-                answer_type='with_full_solutions',
-                include_hints=True,
-                include_instructions=True,
-            )
-        )
-
-        self.assertEqual(
-            recipe.section_types,
-            (
-                HEADER_SECTION,
-                TASK_LIST_SECTION,
-                ANSWERS_SECTION,
-                SHORT_SOLUTIONS_SECTION,
-                FULL_SOLUTIONS_SECTION,
-            ),
-        )
-        self.assertEqual(
-            recipe.sections[1].options,
-            {
-                'include_hints': True,
-                'include_instructions': True,
-            },
-        )
-
-    def test_default_remedial_sheet_recipe_contains_review_and_training(self):
-        recipe = build_remedial_sheet_document_recipe()
-
-        self.assertEqual(recipe.document_type, REMEDIAL_SHEET_DOCUMENT_TYPE)
-        self.assertEqual(
-            recipe.section_types,
-            (
-                HEADER_SECTION,
-                ORIGINAL_MISTAKES_SECTION,
-                TRAINING_TASKS_SECTION,
-                ANSWERS_SECTION,
-                SHORT_SOLUTIONS_SECTION,
-            ),
-        )
-        self.assertEqual(
-            recipe.sections[1].options,
-            {'include_scores': True},
-        )
-        self.assertEqual(
-            recipe.sections[2].options,
-            {'include_scores': False},
-        )
-
-    def test_remedial_sheet_recipe_maps_full_solutions(self):
-        recipe = build_remedial_sheet_document_recipe(
-            RemedialSheetBuildOptions(answer_type='with_full_solutions')
-        )
-
-        self.assertEqual(
-            recipe.section_types,
-            (
-                HEADER_SECTION,
-                ORIGINAL_MISTAKES_SECTION,
-                TRAINING_TASKS_SECTION,
-                ANSWERS_SECTION,
-                SHORT_SOLUTIONS_SECTION,
-                FULL_SOLUTIONS_SECTION,
-            ),
         )

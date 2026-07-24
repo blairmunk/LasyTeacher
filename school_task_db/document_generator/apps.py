0e7051c2fe1dadf7ea87d28b342b4f6034cadbf3
Alexander L. <lemportpro@mail.ru>
2026-07-24 13:23:34 +0300

    Rename document generator admin label

--- school_task_db/document_generator/apps.py
+++ school_task_db/document_generator/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class DocumentTemplateConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'document_generator'
-    verbose_name = 'Document Templates'
+    verbose_name = 'Профили печати'
